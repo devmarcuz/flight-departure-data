71258d950619556b003e02c0cb82c0ef99181466
Otunba Marcus <otunbamarcusademola@gmail.com>
2023-05-07 18:17:06 +0100

    updated route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { loadApiUsers } from "./api/ApiRoutes";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    loadApiUsers();
+  }, []);
+
   return (
     <Router>
       <div className="app">
--- src/api/ApiRoutes.js
+++ src/api/ApiRoutes.js
@@ -1,5 +1,15 @@
-export const host = "https://flight-departure-auth-api.onrender.com";
+import axios from "axios";
+
+// export const host = "https://flight-departure-auth-api.onrender.com";
+export const host = "http://localhost:5000";
 // export const host = "https://swift-chat-app-api.onrender.com";
 
 export const addUserApi = `${host}/api/auth/add-user`;
 export const loginUserApi = `${host}/api/auth/login-user`;
+
+export const loadApiUsers = () => {
+  axios
+    .get(`${host}/api/auth/`)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
